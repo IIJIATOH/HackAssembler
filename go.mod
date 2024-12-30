585ab671fd52f908e97fff001b32c90a7a60e7a0
BaronDeFitenbah <119754277+BaronDeFitenbah@users.noreply.github.com>
2024-12-30 22:08:03 +0500

    Сделал ассемблер, не переделывал код

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/IIJIATOH/HackAssembler
+
+go 1.23.4
---
+++ internal/mypackage/mypackage_test.go
@@ -0,0 +1,9 @@
+// internal/mypackage/mypackage_test.go
+package mypackage
+
+import "testing"
+
+func TestMyFunction(t *testing.T) {
+	MyFunction()
+	// Добавьте проверки здесь
+}
